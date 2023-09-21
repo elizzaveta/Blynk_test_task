3f42baf7e8683a244a6412402874588e65206145
Yelizaveta Timchenko <yelizaveta.timchenko21@gmail.com>
2023-09-21 14:34:24 +0300

    Changed app name, webpage title and icon

--- src/components/side-bar/SideBar.js
+++ src/components/side-bar/SideBar.js
@@ -4,7 +4,7 @@ import styles from '../../assets/css/side-bar/SideBar.module.css'
 const SideBar = () => {
     return (
         <div className={styles.wrapper}>
-            <h1>Test Task</h1>
+            <h1>Comments App</h1>
             <p>Comments with no sense</p>
         </div>
     );
